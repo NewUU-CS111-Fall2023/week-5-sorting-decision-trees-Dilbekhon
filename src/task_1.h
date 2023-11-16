36195dbd78b7354dc1d7b47320ef2dcd62d01347
Dilbekhon <127374324+Dilbekhon@users.noreply.github.com>
2023-11-16 10:27:48 -0500

    Update task_1.h

--- src/task_1.h
+++ src/task_1.h
@@ -1,5 +1,35 @@
-/*
- * Author:
- * Date:
- * Name:
- */
+#include <iostream>
+#include <vector>
+#include <string>
+
+void bubbleSort(std::vector<std::string>& bookTitles) {
+    int n = bookTitles.size();
+
+    for (int i = 0; i < n - 1; ++i) {
+        for (int j = 0; j < n - i - 1; ++j) {
+            // Compare adjacent book titles and swap if they are in the wrong order
+            if (bookTitles[j] > bookTitles[j + 1]) {
+                std::swap(bookTitles[j], bookTitles[j + 1]);
+            }
+        }
+    }
+}
+
+int main() {
+    std::vector<std::string> bookTitles = {
+        "Catcher in the Rye",
+        "Pride and Prejudice",
+        "To Kill a Mockingbird",
+        "The Great Gatsby",
+        "Moby Dick"
+    };
+
+    bubbleSort(bookTitles);
+
+    std::cout << "Sorted list of book titles:" << std::endl;
+    for (const auto& title : bookTitles) {
+        std::cout << title << std::endl;
+    }
+
+    return 0;
+}
